2305a4909ab99d74118007568cd6d0e46ed1a845
Ini Zuplek <zulfancoy82@gmail.com>
2021-03-16 22:47:54 +0700

    edit UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facedes\Hash;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
