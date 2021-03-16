94a7691187dd7fc5092f4fde6f9c11b3940922a3
Ini Zuplek <zulfancoy82@gmail.com>
2021-03-16 23:45:36 +0700

    edit homeblade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="card-body">
+@if (session('status'))
+<div class="alert alert-success" role="alert">
+{{ session('status') }}
+</div>
+@endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+{{__(You are logged in!) }}
+
+<table class="table table-responsive">
+<tr><tr>Username</th><th>:</th><td>{{ $user->username }}</td></tr>
+<tr><tr>Name</th><th>:</th><td>{{ $user->name }}</td></tr>
+<tr><tr>Email</th><th>:</th><td>{{ $user->email }}</td></tr>
+<tr><tr>Created At</th><th>:</th><td>{{ $user->created_at }}</td></tr>
+</table>
 </div>
 @endsection
